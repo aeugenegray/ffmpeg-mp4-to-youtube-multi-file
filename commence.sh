242d77e0dc865e835cb5dbae9036a26ff1e28019
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2019-05-01 21:17:41 -0700

    Update commence.sh

--- commence.sh
+++ commence.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for name in *.mp4; do
-  ffmpeg -i "$name" --c:v libx264 -preset veryfast -maxrate 2500k -bufsize 4000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv -strict -2 rtmp://a.rtmp.youtube.com/live2/tz29-c32s-z8b8-as4r 
-done
+  ffmpeg -re -i "$name" --c:v libx264 -preset veryfast -maxrate 2500k -bufsize 4000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv -strict -2 rtmp://a.rtmp.youtube.com/live2/tz29-c32s-z8b8-as4r 
+done 
